811ef5a928fb2fae2966ac63a11b4c4281f3812e
felipebrasil8 <felipebrga@hotmail.com>
2019-02-23 22:38:10 +0000

    Commint final

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,20 @@ const babiliPlugin = require('babili-webpack-plugin');
 const extractTextPlugin = require('extract-text-webpack-plugin');
 const optimizeCss = require('optimize-css-assets-webpack-plugin');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 let plugins = [];
+
+plugins.push(new HtmlWebpackPlugin({
+	hash: true,
+	minify: {
+		html5: true,
+		collapseWhitespace: true,
+		removeComments: true
+	},
+	filename: 'index.html',
+	template: __dirname + '/main.html'
+}));
 
 plugins.push(new extractTextPlugin('styles.css'));
 
@@ -15,7 +27,15 @@ plugins.push(
     })
 );
 
+
+plugins.push(new webpack.optimize.CommonsChunkPlugin({
+	name: 'vendor',
+	filename: 'vendor.bundle.js'
+}));
+
 if(process.env.NODE_ENV == 'production'){
+	plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
+	
 	plugins.push(new babiliPlugin());
 	plugins.push(new optimizeCss({
 		cssProcessor: require('cssnano'),
@@ -29,11 +49,13 @@ if(process.env.NODE_ENV == 'production'){
 }
 
 module.exports = {
-	entry: './app-src/app.js', // primeiro modulo carregado da minha aplicacao
+	entry: {
+		app: './app-src/app.js',
+		vendor: ['jquery', 'bootstrap', 'reflect-metadata'] 
+	},
 	output: {
 		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-		publicPath: 'dist'
+		path: path.resolve(__dirname, 'dist')
 	},
 	module: {
 		rules:[
